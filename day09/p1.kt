e32aac46aaad4e4e04d0d614803cfa1718d05d05
me@eugenel.in <me@eugenel.in>
2019-08-11 17:10:54 +0100

    Finished part 1 of day 9, might remove magic numbers?

--- day09/p1.kt
+++ day09/p1.kt
@@ -1,2 +1,30 @@
-val players = 10
-val last = 1618
+val players = 400
+val last = 71864
+
+fun main() {
+    var lowest = 1
+    var player = 0
+    var current = 0
+    var scores = Array(players) { 0 }
+    var circle = mutableListOf<Int>()
+    circle.add(0)
+    while (lowest < last) {
+        if (lowest % 23 == 0) {
+            scores[player] += lowest
+            current = (current + circle.size - 7) % circle.size
+            scores[player] += circle[current]
+            circle.removeAt(current)
+        } else {
+            current += 2
+            if (current == circle.size) {
+                circle.add(lowest)
+            } else {
+                current = current % circle.size
+                circle.add(current, lowest)
+            }
+        }
+        lowest++
+        player = (player + 1) % players
+    }
+    println(scores.max())
+}
